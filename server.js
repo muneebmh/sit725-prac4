9ed6972a572ea6f33037399cebaaae2e4772e3a1
muneebmh <muneeb.ulhassan@deakin.edu.au>
2023-12-01 01:31:29 +1100

    Prac4 work

--- server.js
+++ server.js
@@ -1,22 +1,63 @@
 let express = require('express');
 let app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://adminmh:<password>@cluster0.hbefnaa.mongodb.net/?retryWrites=true&w=majority";
 let port = process.env.port || 3000;
+let collection;
 
 app.use(express.static(__dirname + '/'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
-app.get('/', (req, res)=>{
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+async function runDBConnection() {
+    try {
+        await client.connect();
+        collection = client.db().collection('Cat');
+        console.log(collection);
+    } catch(ex) {
+        console.error(ex);
+    }
+}
+
+app.get('/', function (req,res) {
     res.render('index.html');
 });
 
-app.get('/addTwoNumbers',(req,res)=>{
-    let num1 = req.query.number1; // this should return 1
-    let num2 = req.query.number2; // this should return 2
-    let sum = parseInt(num1) + parseInt(num2);
-    let obj = {statusCode:200, message:'success', data:sum}
-
-    res.json(obj);
+app.get('/api/cats', (req,res) => {
+    getAllCats((err,result)=>{
+        if (!err) {
+            res.json({statusCode:200, data:result, message:'get all cats successful'});
+        }
+    });
 });
 
+app.post('/api/cat', (req,res)=>{
+    let cat = req.body;
+    postCat(cat, (err, result) => {
+        if (!err) {
+            res.json({statusCode:201, data:result, message:'success'});
+        }
+    });
+});
+
+function postCat(cat,callback) {
+    collection.insertOne(cat,callback);
+}
+
+function getAllCats(callback){
+    collection.find({}).toArray(callback);
+}
+
 app.listen(port, ()=>{
-    console.log('server started - 2');
+    console.log('express server started');
+    runDBConnection();
 });
